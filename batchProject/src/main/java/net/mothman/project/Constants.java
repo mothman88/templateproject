df2ea126020e8373d98fe3be8ab3c4e3c3c74b27
mothman88 <mothman8.othman@gmail.com>
2013-11-21 11:58:24 +0100

    constants updated

--- batchProject/src/main/java/net/mothman/project/Constants.java
+++ batchProject/src/main/java/net/mothman/project/Constants.java
@@ -7,39 +7,6 @@ package net.mothman.project;
  */
 public class Constants {
 
-	public static final String CATEGORY_ROOT = ""; 
-	public static final String SEPARATOR = "01separatore10";
-	
-	public static final String CATEGORY_COUNT = "counter";
-	public static final String CATEGORY_PATH = "path";
-	public static final String CATEGORY_BREADCRUMB = "breadcrumb";
-	public static final String CATEGORY_PATHID = "pathid";
-	
-	public static final String INTEREST_PATH = "interest";
-	public static final String CANONICAL_PATH = "canonical";
-	public static final String CANONICAL_CATEGORY = "canonicalCategory";
-	
-	// -----------------------------------------------------------------------------------------------------------
-	// TEMPLATE
-	
-	public static final String TEMPLATE_CATEGORY = "template/category.ftl";
-	public static final String TEMPLATE_CATEGORY_VOD = "template/category_vod.ftl";
-	public static final String TEMPLATE_CATEGORY_INTEREST = "template/category_interest.ftl";
-	public static final String TEMPLATE_VOD = "template/vod.ftl";
-	public static final String TEMPLATE_MENU = "template/menu.ftl";
-	public static final String TEMPLATE_COUNTER = "template/counter.ftl";
-	
-	// -----------------------------------------------------------------------------------------------------------
-	// PAGE PATH
-	
-	public static final String PAGE_MENU = "menu.html";
-	public static final String PAGE_COUNTER_SINGLE = "counter_single_column.html";
-	public static final String PAGE_COUNTER_DOUBLE = "counter_double_column.html";
-	public static final String PAGE_INTEREST = "interessanti.html";
-	
-	// -----------------------------------------------------------------------------------------------------------
-	// SQLITE
-	
-	public static String SQLITE_NAME = "catalog.db";
+	public static final String CONSTANTS = "CONSTANTS";
 	
 }
